975b7bd8b21753c0928e6f7d9854d896caa9d13a
helen <helenkokob317@gmail.com>
2025-12-24 18:44:54 +0300

    add basic form handling and Typewriter plug

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+let poemForm = document.querySelector("#form");
+poemForm.addEventListener("submit", generatePoem);
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter(".poem", {
+    strings: "Hello World, welcome to poem generator",
+    autoStart: true,
+    cursor: "",
+    delay: 100,
+  });
+}
